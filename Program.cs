192887b4ebfefa9f7a825d591aaf3f199056dcc2
amarthyapodishetty343-lgtm <amarthyapodishetty343@gmail.com>
2026-04-20 01:37:27 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -47,4 +47,9 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<FlightContext>();
+    db.Database.Migrate();
+}
 app.Run();
